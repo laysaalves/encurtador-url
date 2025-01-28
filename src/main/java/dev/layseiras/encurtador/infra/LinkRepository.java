523315556118b7f1d018861be489f33ce75d5920
Layseiras <layseirasdev@outlook.com>
2025-01-28 11:44:15 -0300

    feat: add Link Repository for connection with database

---
+++ src/main/java/dev/layseiras/encurtador/infra/LinkRepository.java
@@ -0,0 +1,7 @@
+package dev.layseiras.encurtador.infra;
+
+import dev.layseiras.encurtador.domain.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepository extends JpaRepository<Link, Long> {
+}
