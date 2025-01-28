99b007de5a950aac6da014f0b3aae8dae47de433
Layseiras <layseirasdev@outlook.com>
2025-01-28 13:45:35 -0300

    feat: create initial Link service

---
+++ src/main/java/dev/layseiras/encurtador/domain/service/LinkService.java
@@ -0,0 +1,9 @@
+package dev.layseiras.encurtador.domain.service;
+
+import dev.layseiras.encurtador.infra.LinkRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LinkService {
+    private LinkRepository linkRepository;
+}
